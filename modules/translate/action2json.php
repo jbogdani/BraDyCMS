c789b7fa59f1d18870dc254d07b0adbe84057a49
Julian Bogdani <jbogdani@gmail.com>
2012-02-29 11:55:13 +0100

    translate/action2json does not show exception text anymore, but an generic error message. Exceprion text is recorded in error.log

--- modules/translate/action2json.php
+++ modules/translate/action2json.php
@@ -11,18 +11,27 @@ try
 	switch ($_GET['a'])
 	{
 		case 'save':
-			$translate = new translate();
-				
-			if ($translate->save_translation($_GET['context'], $_GET['lang'], $_GET['id'], $_POST, $_GET['t_id']))
+			try
 			{
-				$out->text = "La traduzione è stata salvata";
-				$out->type = 'success';
-				echo json_encode($out);
+				$translate = new translate();
+					
+				if ($translate->save_translation($_GET['context'], $_GET['lang'], $_GET['id'], $_POST, $_GET['t_id']))
+				{
+					$out->text = "La traduzione è stata salvata";
+					$out->type = 'success';
+					echo json_encode($out);
+				}
+				else
+				{
+					throw new MyExc('Non è stato possibile salvare i dati');
+				}
 			}
-			else
+			catch (MyExc $e)
 			{
+				$e->log();
 				throw new MyExc('Non è stato possibile salvare i dati');
 			}
+			
 			break;
 	}
 }

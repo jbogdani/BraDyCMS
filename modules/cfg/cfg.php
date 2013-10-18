56bbd17ef1ff112c0cb329549f30bf32382446f1
Julian Bogdani <jbogdani@gmail.com>
2013-10-18 15:17:51 +0200

    RewiteBase option added in main configuration form

--- modules/cfg/cfg.php
+++ modules/cfg/cfg.php
@@ -45,4 +45,54 @@ class cfg_ctrl extends Controller
 		
 		echo json_encode($ret);
 	}
+  
+  
+  public function empty_trash()
+	{
+		$error = utils::recursive_delete(TMP_DIR, true);
+		
+		if(count($error) > 0)
+		{
+			$ret = array('status' => 'error', 'text' => tr::get('trash_not_emptied') . '. ' . implode('; ', $error));
+		}
+		else
+		{
+			$ret = array('status' => 'success', 'text' => tr::get('trash_emptied'));
+		}
+		
+		echo json_encode($ret);
+	}
+  
+  
+  public function update_htaccess()
+  {
+    $rewriteBase = cfg::get('rewriteBase');
+    
+    if (!$rewriteBase || $rewriteBase === '')
+    {
+      echo json_encode(array('status' => 'success', 'text' => tr::get('htaccess_updated')));
+      return;
+    }
+    
+    $htaccess_arr = file('.htaccess');
+    
+    foreach($htaccess_arr as &$row)
+    {
+      if (strpos($row, 'RewriteBase') === 0)
+      {
+       $row = 'RewriteBase ' . $rewriteBase . "\n";
+      }
+    }
+    
+    if(utils::write_in_file('.htaccess', implode("", $htaccess_arr)))
+		{
+      $ret = array('status' => 'success', 'text' => tr::get('htaccess_updated'));
+		}
+		else
+		{
+      $ret = array('status' => 'error', 'text' => tr::get('htaccess_not_updated'));
+		}
+    echo json_encode($ret);
+    
+  }
 }

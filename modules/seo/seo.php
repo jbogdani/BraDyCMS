529482b88e925741ace03db01cc021a32cfbd5f0
Julian Bogdani <jbogdani@gmail.com>
2014-02-28 22:10:47 +0100

    Sitemap.xml is now handled as module

--- modules/seo/seo.php
+++ modules/seo/seo.php
@@ -37,5 +37,49 @@ EOD;
     header("Content-Type: text/plain");
     echo $robots;
   }
+  
+  
+  public function sitemap()
+  {
+    $home = utils::getBaseUrl();
+    
+    $xml = <<<EOD
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<url>
+  <loc>{$home}</loc>
+  <changefreq>monthly</changefreq>
+</url>
+EOD;
+    
+    $articles = new Article();
+    
+    $all_arts = $articles->getAllValid();
+    
+    if (is_array($all_arts))
+    {
+      foreach ($all_arts as $art)
+      {
+        if ($art['robots'] && (strpos('noindex', $art['robots']) !== false || strpos('nofollow', $art['robots']) !== false ))
+        {
+          continue;
+        }
+        $xml .= "\n" . '<url>'
+          . "\n\t" . '<loc>' . $art['full_url'] . '</loc>'
+          . ($art['updated'] ? "\n\t" . '<lastmod>' . date('Y-m-d', strtotime($art['updated'])) . '</lastmod>' : '')
+          . "\n\t" . '<changefreq>monthly</changefreq>'
+        . "\n". '</url>';
+      }
+    }
+    
+    $xml .= "\n</urlset>";
+    header ("Content-Type:text/xml");
+    echo $xml;
+    
+    }
 }
 ?>

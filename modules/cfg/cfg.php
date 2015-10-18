dde844522db1204b8641994f8226b7be9429c970
Julian Bogdani <jbogdani@gmail.com>
2015-10-18 11:52:16 +0200

    Some integrity controls are performed on cfg data before saving

--- modules/cfg/cfg.php
+++ modules/cfg/cfg.php
@@ -5,35 +5,32 @@
  * @license			MIT, See LICENSE file
  * @since			Feb 23, 2013
  */
- 
+
 class cfg_ctrl extends Controller
 {
 	public function edit()
 	{
 		$data = cfg::get();
-		
+
 		$this->render('cfg', 'form', array( 'data' => $data, 'current_user' => $_SESSION['user_confirmed'], 'is_admin' => $_SESSION['user_admin'] ));
 	}
-	
+
 	public function save()
 	{
 		$post = utils::recursiveFilter($this->post);
-		if (cfg::save($post))
-		{
-			$resp = array('status' => 'success', 'text' => tr::get('ok_cfg_update'));
-		}
-		else
-		{
-			$resp = array('status' => 'error', 'text' => tr::get('error_cfg_update'));
-		}
-		
+
+    $resp = cfg::save($post) ?
+		  array('status' => 'success', 'text' => tr::get('ok_cfg_update'))
+      :
+      array('status' => 'error', 'text' => tr::get('error_cfg_update'));
+
 		echo json_encode($resp);
 	}
-	
+
 	public function empty_cache()
 	{
 		$error = utils::recursive_delete(CACHE_DIR, true);
-		
+
 		if(count($error) > 0)
 		{
 			$ret = array('status' => 'error', 'text' => tr::get('cache_not_emptied') . '. ' . implode('; ', $error));
@@ -42,15 +39,15 @@ class cfg_ctrl extends Controller
 		{
 			$ret = array('status' => 'success', 'text' => tr::get('cache_emptied'));
 		}
-		
+
 		echo json_encode($ret);
 	}
-  
-  
+
+
   public function empty_trash()
 	{
 		$error = utils::recursive_delete(TMP_DIR, true);
-		
+
 		if(count($error) > 0)
 		{
 			$ret = array('status' => 'error', 'text' => tr::get('trash_not_emptied') . '. ' . implode('; ', $error));
@@ -59,11 +56,11 @@ class cfg_ctrl extends Controller
 		{
 			$ret = array('status' => 'success', 'text' => tr::get('trash_emptied'));
 		}
-		
+
 		echo json_encode($ret);
 	}
-  
-  
+
+
   public function update_htaccess()
   {
     if(utils::update_htaccess())
@@ -74,7 +71,7 @@ class cfg_ctrl extends Controller
     {
       $ret = array('status' => 'error', 'text' => tr::get('htaccess_not_updated'));
     }
-    
+
     echo json_encode($ret);
   }
 }

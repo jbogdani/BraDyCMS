645aadcb1e0c09797569d8422f674e6d85a39af3
Julian Bogdani <jbogdani@gmail.com>
2016-01-17 12:05:19 +0100

    log_ctrl::checkAttemptTime moved to utils::checkAttemptTime

--- modules/log/log.php
+++ modules/log/log.php
@@ -5,58 +5,58 @@
  * @license      MIT, See LICENSE file
  * @since      Apr 3, 2013
  */
- 
+
 class log_ctrl extends Controller
 {
-  
+
   private function updateUsersLog($user, $action = ' in')
   {
     $users_log = MAIN_DIR . 'logs/users.log';
-    
+
     // Create users log if it does not exist
     if (!file_exists($users_log))
     {
       $fh = @fopen($users_log, 'w');
       @fclose($fh);
     }
-    
+
     if (!file_exists($users_log))
     {
       return false;
     }
-    
+
     $log_str = $user
       . ' logged ' . $action
       . ' on: ' . date('r') . ' (unix microtime: ' . microtime(true) . ')'
       . ' from IP: ' . $_SERVER['REMOTE_ADDR']
       . "\n";
-          
+
       error_log($log_str, 3, $users_log);
       return true;
   }
-  
+
   public function out()
   {
     $this->updateUsersLog($_SESSION['user_confirmed'], 'out');
-    
+
     $_SESSION['user_confirmed'] = false;
     $_SESSION['user_admin'] = false;
-    
+
     utils::emptyTmp();
-    
+
     session_destroy();
-    
+
     echo '<script>window.location = "./admin"</script>';
   }
-  
+
   public function encodePwd($password = false, $echo = false)
   {
     $password = $password ? $password : $this->request['password'];
-    
+
     $echo = $echo ? $echo : $this->request['echo'];
-    
+
     $password = sha1($password);
-    
+
     if($echo)
     {
       echo $password;
@@ -66,68 +66,40 @@ class log_ctrl extends Controller
       return $password;
     }
   }
-  
-  private function checkAttemptTime()
-  {
-    /**
-     * if file exists read lastAttempt file
-     *  checks if actual IP was the last IP to try to enter
-     *  if so, checks if now isat least 1 sec after last attempt
-     */
-    $logAttempt = MAIN_DIR . 'logs/logAttempts.log';
-    
-    $ip = $_SERVER['REMOTE_ADDR'];
-    
-    $now = microtime(true);
-    
-    if (file_exists($logAttempt))
-    {
-      $lastAttempt = file($logAttempt);
-      $lastIP = str_replace(array("\n", "\r\n"), null, $lastAttempt[0]);
-      $lastTime = floatval(str_replace(array("\n", "\r\n"), null, $lastAttempt[1]));
-      
-      if ($lastIP === $id)
-      {
-        return ($now >= ($lastTime + 1000) );
-      }
-    }
-    
-    return utils::write_in_file($logAttempt, $ip . "\n" . $now);
-  }
-  
+
   public function in()
   {
-    
+
     try
     {
       if (!filter_var($this->post['username'], FILTER_VALIDATE_EMAIL))
       {
         throw new Exception(tr::sget('invalid_email', tr::get('email_address')));
       }
-      
+
       $username = $this->post['username'];
-    
+
       $password = $this->post['password'];
 
       $token = $this->post['token'];
-      
+
       if (!$token || !$username || !$password)
       {
         throw new Exception(tr::get('access_denied'));
       }
-      
+
       if ($token !== $_SESSION['token'])
       {
-        throw new Exception('invalid_token');
+        throw new Exception(tr::get('invalid_token'));
       }
-      
-      if (!$this->checkAttemptTime())
+
+      if (!utils::checkAttemptTime())
       {
         throw new Exception('too_much_attempts');
       }
-      
+
       $cfg_users = cfg::get('users');
-    
+
       foreach ($cfg_users as $user)
       {
         if($username === $user['name'] && $this->encodePwd($password) === $user['pwd'])
@@ -149,15 +121,15 @@ class log_ctrl extends Controller
           continue;
         }
       }
-      
+
       throw new Exception(tr::get('access_denied'));
-      
+
     }
     catch(Exception $e)
     {
       echo json_encode(array('status' => 'error', 'text' => $e->getMessage()));
       return;
     }
-    
+
   }
 }

7f26ad0cb34b11d8493020d90be8eaf8443a162c
Julian Bogdani <jbogdani@gmail.com>
2016-10-23 19:52:29 +0200

    Reload button is showed in error log module even if error file is missing or empty

--- modules/error_log/error_log.php
+++ modules/error_log/error_log.php
@@ -5,36 +5,29 @@
  * @license      MIT, See LICENSE file
  * @since      Dec 11, 2012
  */
- 
+
 class error_log_ctrl
 {
-  private static $file = 'logs/error.log'; 
+  private static $file = 'logs/error.log';
 
   public static function show()
   {
-    $html = '<h2>Error log</h2>';
-    
     if (file_exists(self::$file) AND filesize(self::$file) > 0)
     {
-      $html .= '<div class="btn-group">' .
-            '<a class="btn btn-default log-reload"><i class="icon ion-refresh"></i> Reload</a>' .
-            '<a class="btn btn-default log-delete"><i class="icon ion-trash-a"></i> Delete</a>' .
-            '</div>' .
-            '<hr />';
-    
       $handle = fopen(self::$file, 'r');
       $contents = fread($handle, filesize(self::$file));
       fclose($handle);
-    
-      $html .= '<pre>' . nl2br($contents) . '</pre>';
-      
     }
-    else
-    {
-      $html .= '<div class="text-success"><i class="icon ion-checkmark"></i> Error log is empty</div>';
-      
-    }
-    
+
+    $html = '<h2>Error log</h2>' .
+        '<div class="btn-group">' .
+          '<a class="btn btn-default log-reload"><i class="icon ion-refresh"></i> Reload</a>' .
+          ($contents ? '<a class="btn btn-default log-delete"><i class="icon ion-trash-a"></i> Delete</a>' : '') .
+        '</div>' .
+        '<hr />' .
+        ($contents ? '<pre>' . nl2br($contents) . '</pre>' : '<div class="text-success"><i class="icon ion-checkmark"></i> Error log is empty</div>')
+        ;
+
     echo $html;
     ?>
       <script>
@@ -51,10 +44,10 @@ class error_log_ctrl
         });
       </script>
     <?php
-    
+
   }
-  
-  
+
+
   public static function delete()
   {
     $handle = @fopen(self::$file, 'w+');
@@ -69,7 +62,7 @@ class error_log_ctrl
       $out['status'] = 'error';
       $out['text'] = 'Error! Error-log not deleted';
     }
-    
+
     echo json_encode($out);
   }
 }

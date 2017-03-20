e2dbc857523fd49a122cfc22828505cd77bf5252
Julian Bogdani <jbogdani@gmail.com>
2017-03-20 18:08:22 +0100

    Start development for tmpldata module

---
+++ modules/tmpldata/tmpldata.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @author      Julian Bogdani <jbogdani@gmail.com>
+ * @copyright    BraDyUS. Communicating Cultural Heritage, http://bradypus.net 2007-2013
+ * @license      MIT, See LICENSE file
+ * @since        Aug 29, 2013
+ */
+
+
+class tmpldata_ctrl extends Controller
+{
+  private $data_file = SITE_DIR . 'modules/tmpldata/tmpldata.json';
+
+  public function save()
+  {
+    try
+    {
+
+      if (empty($this->post['data']))
+      {
+        @unlink($this->data_file);
+        if (file_exists($this->data_file))
+        {
+          throw new Exception();
+        }
+      }
+      else
+      {
+        if (!utils::write_in_file($this->data_file, $this->post['data'], 'json'))
+        {
+          throw new Exception();
+        }
+      }
+
+      echo json_encode(array('status' => 'success', 'text' => tr::get('ok_setting_updated') ));
+    }
+    catch (Exception $e)
+    {
+      echo json_encode(array('status' => 'error', 'text' => tr::get('error_setting_not_updated') ));
+    }
+
+
+  }
+
+  public function view()
+  {
+    $this->render('tmpldata', 'edit', array(
+        'data' => (file_exists($this->data_file) ? file_get_contents($this->data_file) : '{}')
+    ));
+  }
+
+
+}
+
+?>

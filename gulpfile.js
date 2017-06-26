569e3a9f3f06ea718433b7020f262b7ea81ce072
Julian Bogdani <jbogdani@gmail.com>
2017-06-26 21:59:27 +0200

    Indentation fixed

--- gulpfile.js
+++ gulpfile.js
@@ -29,49 +29,49 @@ gulp.task('packJS', function(){
 
   // concat already minified files
   gulp.src(minifiedJs)
-    .pipe(concat('minified1.js'))
+  .pipe(concat('minified1.js'))
+  .pipe(gulp.dest('./js/'))
+  .on('end', function(){
+
+    // uglify & concat  plain files
+    gulp.src(changingJS)
+    .pipe(concat('minified2.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('./js/'))
     .on('end', function(){
 
-      // uglify & concat  plain files
-      gulp.src(changingJS)
-        .pipe(concat('minified2.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./js/'))
-        .on('end', function(){
+      // Concat temporary files
+      gulp.src(['./js/minified1.js', './js/minified2.js'])
+      .pipe(concat('admin.min.js'))
+      .pipe(gulp.dest('./js/'))
+      .on('end', function(){
 
-          // Concat temporary files
-          gulp.src(['./js/minified1.js', './js/minified2.js'])
-            .pipe(concat('admin.min.js'))
-            .pipe(gulp.dest('./js/'))
-            .on('end', function(){
-
-              // Delete temporary files
-              del(['./js/minified1.js', './js/minified2.js'])
-                .then(function(){
-                  console.log('Javascript packed!');
-                });
-            });
+        // Delete temporary files
+        del(['./js/minified1.js', './js/minified2.js'])
+        .then(function(){
+          console.log('Javascript packed!');
         });
+      });
     });
+  });
 });
 
 
 
 gulp.task('frontCSS', function () {
   gulp.src('sites/default/css/styles.less')
-    .pipe(less().on('error', function(err){ console.log(err.message); }))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('sites/default/css/'));
-    //.pipe(reload({stream: true}));
+  .pipe(less().on('error', function(err){ console.log(err.message); }))
+  .pipe(minifyCSS())
+  .pipe(gulp.dest('sites/default/css/'));
+  //.pipe(reload({stream: true}));
 });
 
 gulp.task('adminCSS', function () {
   gulp.src('less/admin.less')
-    .pipe(less().on('error', function(err){ console.log(err.message); }))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('css/'));
-    //.pipe(reload({stream: true}));
+  .pipe(less().on('error', function(err){ console.log(err.message); }))
+  .pipe(minifyCSS())
+  .pipe(gulp.dest('css/'));
+  //.pipe(reload({stream: true}));
 });
 
 gulp.task('bower', function(){
@@ -86,7 +86,7 @@ gulp.task('bower', function(){
 
 gulp.task('default', [], function(){
   gulp.watch(changingJS, ['packJS']);
-//  gulp.watch(['index.php', 'sites/default/**/*.twig'], [reload]);
+  //  gulp.watch(['index.php', 'sites/default/**/*.twig'], [reload]);
   gulp.watch(['less/**/*.less', '!less/admin.less'], ['frontCSS', 'adminCSS']);
   gulp.watch(['sites/default/css/*.less'], ['frontCSS']);
   gulp.watch(['less/admin.less'], ['adminCSS']);

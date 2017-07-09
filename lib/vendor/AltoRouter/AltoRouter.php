8e4b876f7e11f7bd66b87ab91b9e779f5e81c811
Julian Bogdani <jbogdani@gmail.com>
2017-07-09 23:07:23 +0200

    Minor typo fix

--- lib/vendor/AltoRouter/AltoRouter.php
+++ lib/vendor/AltoRouter/AltoRouter.php
@@ -41,7 +41,7 @@ class AltoRouter {
 		$this->setBasePath($basePath);
 		$this->addMatchTypes($matchTypes);
 	}
-	
+
 	/**
 	 * Retrieves all routes.
 	 * Useful if you want to process or display routes.
@@ -133,7 +133,7 @@ class AltoRouter {
 
 		// Replace named parameters
 		$route = $this->namedRoutes[$routeName];
-		
+
 		// prepend base path to route url again
 		$url = $this->basePath . $route;
 
@@ -255,7 +255,7 @@ class AltoRouter {
 				}
 
 				$optional = $optional !== '' ? '?' : null;
-				
+
 				//Older versions of PCRE require the 'P' in (?P<named>)
 				$pattern = '(?:'
 						. ($pre !== '' ? $pre : null)

1b34edebd8e991e45046646c7dbf92fd2dcbfbb5
Julian Bogdani <jbogdani@gmail.com>
2017-08-29 15:21:19 +0200

    Code cleaning

--- index.php
+++ index.php
@@ -9,20 +9,20 @@ try
 {
   require_once 'lib/globals.inc';
 
-  if (defined('CREATE_SITE'))
-  {
+  if (defined('CREATE_SITE')) {
     header('location: ./admin');
   }
 
   Router::run();
-}
-catch (Exception $e)
-{
+
+} catch (Exception $e) {
+
   header('Content-Type: text/html; charset=utf-8');
-  if ($_SESSION['debug'])
-  {
+
+  if ($_SESSION['debug']) {
     var_dump($e);
   }
+  
   echo tr::get('error_check_log');
   error_log($e->getMessage());
 }

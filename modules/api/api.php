cc2114899789baa036b88e05c651aac2dc7ad8e7
Julian Bogdani <jbogdani@gmail.com>
2018-08-12 15:46:19 +0200

    Api returns max 200 articles

--- modules/api/api.php
+++ modules/api/api.php
@@ -2,10 +2,10 @@
 
 /**
  * @author      Julian Bogdani <jbogdani@gmail.com>
- * @copyright    BraDyUS. Communicating Cultural Heritage, http://bradypus.net 2007-2014 
+ * @copyright    BraDyUS. Communicating Cultural Heritage, http://bradypus.net 2007-2014
  * @license      All rights reserved
  * @since        Feb 7, 2014
- * 
+ *
  * GET:
  *  action: string, required. Actually only read is supported
  *  lang: string, optional. User language id.
@@ -13,22 +13,22 @@
  *  tag:  string|array, optional. Tag or tags to use to filter articles
  *  artid: string|array, optional. Textid or array of textids of articles that will be returned
  *  metadata: boolean, optional, default false. If true an array with page metadata will be returned
- * 
+ *
  * RESPONSE
  *  menu: [
  *          menuid: [],
  *        ]
  *  tags:[
- *        {article data...}, 
+ *        {article data...},
  *        ]
  *  metadata:[
- *            ..., 
+ *            ...,
  *            ]
  *  article: [
  *              textid: [],
  *            ]
  *  galleries: true|false
- * 
+ *
  */
 
 class api_ctrl extends Controller
@@ -36,19 +36,19 @@ class api_ctrl extends Controller
   public function run()
   {
     $array = $this->process($this->get);
-    
+
     header("Access-Control-Allow-Origin: *");
     header('Content-type: text/json');
     header('Content-type: application/json');
-    
+
     echo json_encode($array, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
   }
-  
-  
+
+
   private function process($get)
   {
     $response = array();
-    
+
     switch($get['action'])
     {
       case 'read':
@@ -62,8 +62,8 @@ class api_ctrl extends Controller
           $response['verbose'] = 'Unknown action: ' . $e->getMessage();
         }
         break;
-      
-      
+
+
       case false:
       default:
         $response['status'] = 'error';
@@ -72,14 +72,14 @@ class api_ctrl extends Controller
     }
     return $response;
   }
-  
-  
+
+
   private function read($get)
   {
-    
+
     $response = array('status' => 'success');
     $out = new Out($get, $get['lang']);
-    
+
     /**
      * Menus
      * menu=menuID or
@@ -95,7 +95,7 @@ class api_ctrl extends Controller
       {
         $menus = $get['menu'];
       }
-      
+
       if (is_array($menus))
       {
         foreach ($menus as $menu)
@@ -104,13 +104,13 @@ class api_ctrl extends Controller
         }
       }
     }
-    
+
     /**
      * Tags
      * tag=tagID or
      * tag[]=firstTagId&tag[]=secondTagId...
      */
-    
+
     if ($get['tag'])
     {
       if (is_string($get['tag']))
@@ -123,12 +123,12 @@ class api_ctrl extends Controller
       }
       if (is_array($tags))
       {
-        $arts = $out->getArticlesByTagArray($tags);
+        $arts = $out->getArticlesByTagArray($tags, false, false, 200);
         if (is_array($arts) && !empty($arts))
         {
           $response['tags'] = array_values($arts);
         }
-        
+
         if ($get['galleries'] && !empty($response['tags']))
         {
           foreach($response['tags'] as &$art)
@@ -142,11 +142,11 @@ class api_ctrl extends Controller
               error_log($e->getMessage());
             }
           }
-            
+
         }
       }
     }
-    
+
     /**
      * Page metadata
      */
@@ -154,7 +154,7 @@ class api_ctrl extends Controller
     {
       $response['metadata'] = $out->getPageData();
     }
-    
+
     /**
      * Article
      */
@@ -168,21 +168,21 @@ class api_ctrl extends Controller
       {
         $artids = $get['artid'];
       }
-      
+
       if (is_array($artids))
       {
         foreach($artids as $artid)
         {
           $art = false;
           $art = $out->getArticle($artid);
-      
+
           if($art instanceof RedBean_OODBBean)
           {
             $art = $art->export();
           }
-          
+
           $response['article'][$artid] = $art;
-          
+
           if ($get['galleries'])
           {
             try
@@ -193,12 +193,12 @@ class api_ctrl extends Controller
             {
               error_log($e->getMessage());
             }
-            
+
           }
         }
       }
     }
-    
+
     return $response;
   }
 }

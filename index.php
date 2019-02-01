f34d4b5f5b99bb5ee0186a38179a848543699380
Julian Bogdani <jbogdani@gmail.com>
2019-02-01 18:23:53 +0100

    Back compatibility with php 5.6

--- index.php
+++ index.php
@@ -7,46 +7,51 @@
  */
  ob_start();
 
- require_once 'lib/vendor/phpfastcache/lib/Phpfastcache/Autoload/Autoload.php';
-
- use Phpfastcache\CacheManager;
- use Phpfastcache\Config\Config;
- use Phpfastcache\Core\phpFastCache;
-
 try {
 
-  CacheManager::setDefaultConfig(new Config([
-    "path" => sys_get_temp_dir(),
-    "itemDetailedDate" => false
-  ]));
+  // with PHP higher or equal 7 run Phpfastcache
+  if (version_compare(phpversion(), '7.0.0') > -1){
+    require_once 'lib/vendor/phpfastcache/lib/Phpfastcache/Autoload/Autoload.php';
 
-  $InstanceCache = CacheManager::getInstance('files');
-  $key = md5($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['QUERY_STRING']);
-  $CachedString = $InstanceCache->getItem($key);
-  require_once 'lib/Bootstrap.php';
-  if ( $_SESSION['debug']
-    || preg_match('/\.map/', $_SERVER['REQUEST_URI'])
-    || preg_match('/\.json/', $_SERVER['REQUEST_URI'])
-    || preg_match('/controller\.php/', $_SERVER['REQUEST_URI'])
-    || $_SERVER['REQUEST_URI'] === '/admin'
-    || preg_match('/\.draft$/', $_SERVER['REQUEST_URI'])
-    || is_null($CachedString->get())
-  ) {
+    \Phpfastcache\CacheManager::setDefaultConfig(new \Phpfastcache\Config\Config([
+      "path" => sys_get_temp_dir(),
+      "itemDetailedDate" => false
+    ]));
 
+    $InstanceCache = \Phpfastcache\CacheManager::getInstance('files');
+    $key = md5($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['QUERY_STRING']);
+    $CachedString = $InstanceCache->getItem($key);
+    require_once 'lib/Bootstrap.php';
+    if ( $_SESSION['debug']
+      || preg_match('/\.map/', $_SERVER['REQUEST_URI'])
+      || preg_match('/\.json/', $_SERVER['REQUEST_URI'])
+      || preg_match('/controller\.php/', $_SERVER['REQUEST_URI'])
+      || $_SERVER['REQUEST_URI'] === '/admin'
+      || preg_match('/\.draft$/', $_SERVER['REQUEST_URI'])
+      || is_null($CachedString->get())
+    ) {
+
+      Router::run();
+
+      $html = ob_get_contents();
+      ob_end_clean();
+
+      $CachedString->set($html)->expiresAfter(60);
+      $InstanceCache->save($CachedString);
+
+    } else {
+
+      $html = $CachedString->get();
+    }
+
+    echo $html;
+
+  } else {
     Router::run();
 
-    $html = ob_get_contents();
-    ob_end_clean();
-
-    $CachedString->set($html)->expiresAfter(60);
-    $InstanceCache->save($CachedString);
-    
-  } else {
-
-    $html = $CachedString->get();
   }
 
-  echo $html;
+
 
 } catch (Exception $e) {
 
